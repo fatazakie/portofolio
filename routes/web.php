b3541d777925991be7d3be3d5759958ede29f09b
fatazakie <160807919+fatazakie@users.noreply.github.com>
2026-01-18 18:30:07 +0700

    Revert "Portolio Pribadi"
    This reverts commit d3b81c12f9ff82e7d66c1ec605f7d55ecd5c6b4d.

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('portfolio');
-});
-Route::get('/download-cv', function () {
-    return response()->download(storage_path('app/public/cv/fatazakie.pdf'));
+    return view('welcome');
 });
